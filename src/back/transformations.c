8f70344ac0ac42db19831a6015ae8f86e68ca4cf
Macy Gabrielle <macygabr@ob-h4.nsk.21-school.ru>
2023-08-23 17:38:09 +0700

    scaling add

--- src/back/transformations.c
+++ src/back/transformations.c
@@ -5,36 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int readFile(char* filename, dataNur* outputdata) {
-  FILE* fp;
-  size_t len = 0;
-  int vertexes = 0;
-  int facets = 0;
-  int vertexesNum = 0;
-  int facetsNum = -1;
-  char* buffer = NULL;
-  outputdata->count_of_vertexes = 0;
-  outputdata->count_of_facets = 0;
-  fp = fopen(filename, "r");
-  if (fp != NULL) {
-    countSize(fp, &vertexes, &facets);
-    outputdata->vertexesArr = (double*)calloc(vertexes, sizeof(double));
-    outputdata->facetsArr = (int*)calloc(facets*2, sizeof(int));
-
-    while (getline(&buffer, &len, fp) != -1) {
-      if (buffer[0] == 'v' && buffer[1] == ' ')
-        parsVertexes(outputdata, &vertexesNum, buffer);
-      if (buffer[0] == 'f' && buffer[1] == ' ')
-        parsFacets(outputdata, &facetsNum, buffer);
-    }
-  }
-
-  free(buffer);
-  fclose(fp);
-  return fp == NULL ? 1 : 0;
-}
-
-int rotate(dataNur* inputdata, int degree, char axis) {
+int rotateObj(dataNur* inputdata, int degree, char axis) {
   int add1 = 0, add2 = 0, error = 0;
   switch (axis) {
     case 'x':
@@ -65,70 +36,11 @@ int rotate(dataNur* inputdata, int degree, char axis) {
   return degree > 359 ? error : 1;
 }
 
-int parsVertexes(dataNur* outputdata, int* vertexesNum, char* buffer) {
-  for (int i = 1; buffer[i] != '\n' && buffer[i] != '\0'; i++)
-    if (buffer[i] != ' ')
-      outputdata->vertexesArr[(*vertexesNum)++] = makeNum(buffer, &i);
-  outputdata->count_of_vertexes++;
-  return 0;
+int scalingObj(dataNur* inputdata, double scale){
+  for (int i = 0; i < inputdata->count_of_vertexes; i ++) inputdata->vertexesArr[i] *= scale;
+  return scale>0 ? 0:1;
 }
 
-int parsFacets(dataNur* outputdata, int* facetsNum, char* buffer) {
-  int first = 0;
-  int block = 0;
-  for (int i = 1; buffer[i] != '\n' && buffer[i] != '\0'; i++) {
-    if (buffer[i - 1] == ' ') {
-      outputdata->facetsArr[++(*facetsNum)] = (int)(buffer[i] - '0');
-      if (first) outputdata->facetsArr[++(*facetsNum)] = (int)(buffer[i] - '0');
-      if (!first) first = (int)(buffer[i] - '0');
-    }
-  }
-
-  outputdata->facetsArr[++(*facetsNum)] = first;
-  outputdata->count_of_facets++;
-  return 0;
-}
-
-double makeNum(char* content, int* i) {
-  double res = 0.0;
-  int j = 0;
-  int flag = 0;
-  int dot = 0;
-  int minus = 0;
-
-  for (; content[(*i)] != ' ' && content[(*i)] != '\0' && content[(*i)] != '\n';
-       (*i)++) {
-    if (content[(*i)] == '.') {
-      flag = j;
-      dot++;
-    }
-    if (content[(*i)] >= '0' && content[(*i)] <= '9') {
-      j++;
-      res += (double)(content[(*i)] - '0') / ((int)pow(10, j));
-    }
-    if (content[(*i)] == '-') {
-      minus = 1;
-    }
-  }
-  if (!flag) flag = j;
-  res *= pow(10, (flag));
-  if (minus) res *= -1;
-  return res;
-}
-
-int countSize(FILE* fp, int* vertexes, int* facets) {
-  char* line = NULL;
-  size_t len = 0;
-  while (getline(&line, &len, fp) != -1) {
-    if (line[0] == 'v' && line[1] == ' ')
-      for (int i = 1; i < strlen(line); i++)
-        if (line[i] == ' ') (*vertexes)++;
-
-    if (line[0] == 'f' && line[1] == ' ')
-      for (int i = 1; i < strlen(line); i++)
-        if (line[i] == ' ') (*facets)++;
-  }
-  rewind(fp);
-  free(line);
+int shiftObj(dataNur* inputdata, int shift, char axis){
   return 0;
 }
--- src/back/transformations.h
+++ src/back/transformations.h
@@ -3,32 +3,19 @@
 #include <stdio.h>
 
 typedef struct {
-  double **matrix;
-  int rows;
-  int columns;
-} matrix_t;
-
-typedef struct {
-    int i;
-} polygon_t;
-
-typedef struct {
 	int count_of_vertexes;
 	int count_of_facets;
 	double* vertexesArr;
 	int* facetsArr;
 } dataNur;
 
-dataNur* transformationsMatrix();
-int outputData();
-int moveMatrix();
-int turnMatrix();
-int scalingMatrix();
 int readFile(char* filename, dataNur* outputdata);
-int mult_matrix();
 int parsVertexes(dataNur* outputdata, int *index, char* buffer);
 int parsFacets(dataNur* outputdata, int *index, char* buffer);
 double makeNum(char* content, int* i);
-int create_matrix(int rows, int columns, matrix_t *result);
 int countSize(FILE* fp,int* vertexes, int* facets);
+
+int rotateObj(dataNur* inputdata, int degree, char axis);
+int scalingObj(dataNur* inputdata, double scale);
+int shiftObj(dataNur* inputdata, int shift, char axis);
 #endif
